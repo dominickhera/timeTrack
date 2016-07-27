9774feffb6d1c52a08ddb1846186240b2ca88e1d
Dominick <dominickhera@me.com>
2016-07-27 13:42:22 -0400

    minor changes, gonna make big revisions soon

--- timeTrack/ViewController.swift
+++ timeTrack/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var passButton: UIButton!
+    @IBOutlet var cashButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func passButtonAction(sender: AnyObject) {
+        print("pass is best")
+    }
 
+    @IBAction func cashButtonAction(sender: AnyObject) {
+        print("cash is best")
+    }
 }
 
